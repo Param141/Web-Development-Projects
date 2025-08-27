056473f253c0afa7e13b024bd97567847973bdbe
Param141 <paramsharma802@gmail.com>
2025-08-27 18:07:24 +0530

    Made a Analog Clock using HTML,CSS,JS

---
+++ analog_clock/app.js
@@ -0,0 +1,14 @@
+setInterval(() => {
+    d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+    hrotation = 30*htime + mtime/2;
+    mrotation = 6*mtime;
+    srotation = 6*stime;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;   //using transform and rotate property
+    second.style.transform = `rotate(${srotation}deg)`;
+}, 1000);  
+ // repeat this work after every one sec setInterval is doing that thing
